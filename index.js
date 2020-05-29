c4b4f29e401cb2fc0968e229f389de66f8289475
Craig Cooper <c@c9r.dev>
2020-05-29 10:41:44 +0100

    Support new base stack name

--- index.js
+++ index.js
@@ -2,26 +2,28 @@ const core = require("@actions/core");
 const AWS = require("aws-sdk");
 const cloudformation = new AWS.CloudFormation({region: process.env.AWS_DEFAULT_REGION});
 
+const BASE_STACK_NAME = "tros";
+
 async function run() {
     try {
         let stacks = core.getInput("stacks" );
         const branch = process.env.GITHUB_REF.split("/").pop();
         core.setOutput("branch", branch);
 
-        if(!stacks) stacks = "base"
-        if(!stacks.includes("base")) stacks = `base,${stacks}`
+        if(!stacks) stacks = BASE_STACK_NAME
+        if(!stacks.includes(BASE_STACK_NAME)) stacks = `${BASE_STACK_NAME},${stacks}`
 
         const {Stacks} = await cloudformation.describeStacks().promise();
 
         const outputs = Stacks.reduce((acc, el) => {
-            if(el.StackName.startsWith("base")) {
-              acc.base = acc.base.concat(el.Outputs);
+            if(el.StackName.startsWith(BASE_STACK_NAME)) {
+              acc[BASE_STACK_NAME] = acc.tros.concat(el.Outputs);
             } else {
               acc[el.StackName] = el.Outputs
             }
 
             return acc;
-        }, {base:[]})
+        }, {[BASE_STACK_NAME]:[]})
 
         for(const stack of stacks.split(",")) {
             core.info(`Getting outputs from ${stack.trim()}`);
